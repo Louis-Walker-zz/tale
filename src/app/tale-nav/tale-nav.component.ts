5965b4fe542a9e17a6947cec118290788bf4eff5
Louis Walker <phetablack@gmail.com>
2016-09-15 13:22:34 +0100

    tale-nav component styling, semantics, and edgey new method

--- src/app/tale-nav/tale-nav.component.ts
+++ src/app/tale-nav/tale-nav.component.ts
@@ -7,10 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tale-nav.component.css']
 })
 export class NavComponent {
+  taleNav: Object;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    // Temp CFG
+    this.taleNav = [
+      {
+        name: "Dashboard",
+        permalink: "dashboard",
+        action: "createAppeal"
+      },
+      {
+        name: "Appeals",
+        permalink: "appeals",
+        action: "createAppeal"
+      },
+      {
+        name: "Leads",
+        permalink: "leads",
+        action: "createLead"
+      },
+      {
+        name: "Users",
+        permalink: "users",
+        action: "inviteUser",
+        admin: true
+      }
+    ]
   }
 
+  linkify(permalink): string {
+    return "http://" + window.location.hostname + ":" + window.location.port + "/" + permalink;
+  }
 }
