7a3ce8a980a9c4c2b7928378ff6d2d599a0039f0
Louis Walker <phetablack@gmail.com>
2016-09-19 15:17:50 +0100

    potentials filter component, potentials component foundation

--- src/app/tale-potentials/tale-potentials-filter/tale-potentials-filter.component.ts
+++ src/app/tale-potentials/tale-potentials-filter/tale-potentials-filter.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'tale-potentials-filter',
   templateUrl: 'tale-potentials-filter.component.html',
   styleUrls: ['tale-potentials-filter.component.css']
 })
 export class PotentialsFilterComponent {
+  filterOptions: Object;
 
-  constructor() { }
+  constructor() {
+    this.filterOptions = {
+      "regions": {
+        "northWest": {
+          "shortName": "nw",
+          "enabled": true
+        },
+        "southWest": {
+          "shortName": "sw",
+          "enabled": true
+        },
+        "northEast": {
+          "shortName": "ne",
+          "enabled": true
+        },
+        "southEast": {
+          "shortName": "se",
+          "enabled": true
+        },
+      },
+      "orderNames": [
+        "Most Recent",
+        "Most Popular",
+        "Most Potential"
+      ]
+    }
+
+    this.filterOptions["currentOrder"] = this.filterOptions["orderNames"][0];
+  }
+
+  setOrder(name) {
+    let options = this.filterOptions;
+
+    options["currentOrder"] = name;
+  }
 
 }
