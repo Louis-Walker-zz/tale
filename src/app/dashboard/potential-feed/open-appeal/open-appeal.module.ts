ad492814fd6b24d5104e029387a57efea4ffafa8
Louis Walker <phetablack@gmail.com>
2016-10-02 00:25:12 +0100

    Added ng2-googlemaps

--- src/app/dashboard/potential-feed/open-appeal/open-appeal.module.ts
+++ src/app/dashboard/potential-feed/open-appeal/open-appeal.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }   from '@angular/forms';
 
 import { MyDatePickerModule } from 'mydatepicker/src/my-date-picker/my-date-picker.module';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { OpenAppealComponent } from './open-appeal.component';
 
@@ -10,7 +11,10 @@ import { OpenAppealComponent } from './open-appeal.component';
   imports: [
     CommonModule,
     FormsModule,
-    MyDatePickerModule
+    MyDatePickerModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAlQFAxlMTwSnTJ6nXsuBoyMRN1VtmWER0'
+    })
   ],
   declarations: [OpenAppealComponent],
   exports: [
